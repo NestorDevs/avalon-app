c144d75f306baa558efc0f947dc95f9d8d11782b
NestorDevs <neivertjosias@gmail.com>
2022-06-17 22:08:28 -0400

    add data sources, models repositories, tables for hive

---
+++ lib/data/tables/movie_table.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: overridden_fields
+
+import 'package:hive/hive.dart';
+
+import '../../domain/entities/movie_entity.dart';
+
+part 'movie_table.g.dart';
+
+@HiveType(typeId: 0)
+class MovieTable extends MovieEntity {
+  @override
+  @HiveField(0)
+  final int id;
+
+  @override
+  @HiveField(1)
+  final String title;
+
+  @override
+  @HiveField(2)
+  final String posterPath;
+
+  const MovieTable({
+    required this.id,
+    required this.title,
+    required this.posterPath,
+  }) : super(
+          id: id,
+          title: title,
+          posterPath: posterPath,
+          backdropPath: '',
+          releaseDate: '',
+          voteAverage: 0,
+        );
+
+  factory MovieTable.fromMovieEntity(MovieEntity movieEntity) {
+    return MovieTable(
+      id: movieEntity.id,
+      title: movieEntity.title,
+      posterPath: movieEntity.posterPath,
+    );
+  }
+}
